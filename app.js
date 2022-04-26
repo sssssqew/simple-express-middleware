83ecb5da3441c0b7f4f018ae3f34d8ad49513852
sssssqew <sunrise.in.my.dream@gmail.com>
2022-04-26 19:36:27 +0900

    순수 자바스크립트로 간단한 express 미들웨어 구현

---
+++ app.js
@@ -0,0 +1,96 @@
+// DOM 조회하기
+const content = document.getElementById('content')
+const pageBtn = document.getElementById('page-btn')
+
+// 라우터 (router)
+const urls = []
+
+// 라우트(routes)
+const routes = ['/', '/read', '/create', '/update', '/delete', '/test', '/notFound']
+
+// 현재 URL 주소
+let currentUrl = routes[0]
+
+// 요청(request) 객체
+const req = {
+  method: 'GET',
+  contentType: 'application/json',
+  host: 'localhost:5000' 
+}
+
+// 응답(response) 객체
+const res = {
+  send: (input) => content.innerHTML = input,
+  json: (input) => JSON.stringify(input)
+}
+
+const next = () => {}
+
+// 미들웨어 함수
+const app = {
+  get: (url, callback) => urls.push({
+    url, callback
+  }),
+  post: (url, callback) => urls.push({
+    url, callback
+  }),
+  put: (url, callback) => urls.push({
+    url, callback
+  }),
+  delete: (url, callback) => urls.push({
+    url, callback
+  }),
+  use: (callback) => urls.push({
+    callback
+  })
+}
+
+// 미들웨어 등록
+app.get('/', (req, res) => {
+  res.send('<h1>This is HOME page !</h1>')
+})
+app.get('/read', (req, res) => {
+  res.send('<h1>This is READ page !</h1>')
+})
+app.post('/create', (req, res) => {
+  res.send('<h1>This is CREATE page !</h1>')
+})
+app.put('/update', (req, res) => {
+  res.send('<h1>This is UPDATE page !</h1>')
+})
+app.delete('/delete', (req, res) => {
+  res.send('<h1>This is DELETE page !</h1>')
+})
+app.get('/test', (req, res) => {
+  res.send(`
+    <h1>Test Page</h1>
+    <p>This is test page</p>
+    <ul>
+      <li>apple</li>
+      <li>banana</li>
+      <li>orange</li>
+    </ul>
+  `)
+})
+app.use((req, res, next) => {
+  res.send('<h1>404 page</h1>')
+})
+
+console.table(urls) // 라우트 저장
+
+// 페이지 변경하기
+const changePage = (currentUrl) => {
+  console.log(currentUrl)
+  const urlSelected = urls.filter(url => url.url === currentUrl)[0] // 현재 URL 에 대한 라우트 선택
+  urlSelected ? urlSelected.callback(req, res) : urls[urls.length-1].callback(req, res, next) // 라우터 핸들러 실행 
+}
+
+// 이벤트 핸들러 정의
+window.addEventListener('DOMContentLoaded', () => {
+  changePage(currentUrl)
+})
+pageBtn.addEventListener('click', () => {
+  const currentUrl = routes[Math.floor(Math.random()*routes.length)]
+  changePage(currentUrl)
+})
+
